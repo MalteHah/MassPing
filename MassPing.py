a63404acca2f8475c9bc6cfc4d6554d913eb3ad5
MalteHah <malte@gissmo.eu>
2018-12-28 12:36:16 +0100

    cleanups, indent, logging

--- MassPing.py
+++ MassPing.py
@@ -1,18 +1,21 @@
+#!/usr/local/bin/python3.6
+
 """
 MassPing.py
 
 script loading a file with a list of devices to ping, then pings the devices, then puts
 the results into a webpage
 
-v1	2017-1217	jadavis Initial version
-v2  2018-0118	jadavis	Updated to tag influxdb data with hostname as supplied in devicelist;
-also removed HTML web page creation in lieu of using Grafana
+v1      2017-1217       jadavis Initial version
+v2  2018-0118   jadavis Updated to tag influxdb data with hostname as supplied in devicelist;
+                                                also removed HTML web page creation in lieu of using Grafana
 
-Forked Version Malte Hahlbeck
+TODO: 
 
-- code cleanups
-- logging
-- pysnmp, queue utilization, QoS
+* devicelist wird vor jedem Ping neu eingelesen. Bug oder feature? 
+  --> Feature! Denn sonst müsste man bei jeder Änderung den Dienst neu starten.
+* stderr einfangen über den logger
+* logrotate mit logger --> erledigt
 
 """
 
@@ -20,18 +23,20 @@ import shlex
 from subprocess import check_output, Popen, PIPE
 import re
 import requests
-import sys  # needed for sys.exit
-import schedule
+# import sys #needed for sys.exit
+# import schedule
 import time
 import datetime
+# import logging
+import logging.handlers
 
 ## User defined vars
 devicelist = "devicelist.txt"
 influxserver = "localhost"  # hostname or IP of InfluxDB server
-databasename = "CHANGEME"  # name of existing InfluxDB database
-
-
+databasename = "MassPing"  # name of existing InfluxDB database
 ##
+
+logger = logging.getLogger('MassPing')
 
 
 def get_fping_output(cmd):
@@ -49,29 +54,30 @@ def load_devicefile():
             line = line.strip()  # preprocess line
             ipaddress, hostname, location, function = line.split()
             iplist[ipaddress] = [hostname, location, function]
+    logger.info('Loading devicelist')
     return iplist
 
 
 def getpingresults():
     iplist = dict(load_devicefile())
-    cmd = "/usr/bin/fping -C 1 -A -q {}".format(" ".join(map(str, iplist.keys())))
+    cmd = "/usr/local/sbin/fping -C 3 -A -q {}".format(" ".join(map(str, iplist.keys())))
     exitcode, out, results = get_fping_output(cmd)
 
     pingresults = []
     for aline in results.split("\n"):
-        # print('Working on line: {0}'.format(aline))
+        logger.debug('Working on line: {0}'.format(aline))
         if aline:
             m = re.match(r"(\S+)\s+:\s(\S+)", aline)
             ipaddress = m.group(1)
             rtt = m.group(2)
             if rtt == '-':
                 iplist[ipaddress] += (float(9999),)
-            elif float(rtt) > warningratio:
-                iplist[ipaddress] += (float(rtt),)
+            #                       elif float(rtt) > warningratio:
+            #                               iplist[ipaddress] += (float(rtt),)
             else:
                 iplist[ipaddress] += (float(rtt),)
 
-    # print(iplist)
+    logger.info('get ping results')
     return iplist
 
 
@@ -88,7 +94,7 @@ def createtabledata():
         influxdata.append(influxentry)
 
     influxdata = '\n'.join(influxdata)
-    # print(influxdata)
+    logger.info('ab in die DB damit')
     return influxdata
 
 
@@ -99,27 +105,50 @@ def write2influx():
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
     }
-
     response = requests.request("POST", url, data=influxdata, headers=headers, params=params)
+    print(response.text)
 
 
-# print(response.text)
+# def write2influx():
+#       influxdata = createtabledata()
+#       url = "http://10.3.42.110:8086/write"
+#       params = {"db":databasename}
+#       headers = {
+#       'Content-Type': "application/x-www-form-urlencoded",
+#       }
+#        
+#        response = requests.request("POST", url, data=influxdata, headers=headers, params=params)
+#       print(response.text)
+
 
 def dowork():
     timenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     start = datetime.datetime.now()
-    print("Started...", timenow)
+    # logger.info("Started...", timenow)
+    logger.info("Started...")
     write2influx()
     end = datetime.datetime.now()
     elapsed = end - start
     # print(elapsed.seconds,":",elapsed.microseconds)
-
     # timenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    print("   Done... {0}.{1}sec".format(elapsed.seconds, elapsed.microseconds))
+    # logger.info("Done... {0}.{1}sec".format(elapsed.seconds, elapsed.microseconds))
+    logger.info("Done...")
 
 
-schedule.every(10).seconds.do(dowork)
+log_filename = '/var/log/MassPing.log'
+file_handler = logging.handlers.RotatingFileHandler(log_filename,
+                                                    mode='a',
+                                                    maxBytes=2000000,
+                                                    backupCount=5)
+fmt = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+file_handler.setFormatter(fmt)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+# TODO: macht noch Probleme mit logger
+# schedule.every(10).seconds.do(dowork)
+
+while True:
+    # schedule.run_pending()
+    dowork()
+    time.sleep(10)
